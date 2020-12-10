51969a80f416459cad5411d412aa1274e01f8231
danielepupilli <daniele01.pupilli@studenti.unicam.it>
2020-12-10 11:31:12 +0100

    Revert "inserimento metodi per selezione progettisti"
    This reverts commit 0cd5dfa3e18e6adbdefe6544af81ca08dcdc952e.

--- src/main/java/it/unicam/cs/ids/DOIT.java
+++ src/main/java/it/unicam/cs/ids/DOIT.java
@@ -1,14 +1,4 @@
 package it.unicam.cs.ids;
 
 public class DOIT {
-<<<<<<< Updated upstream
-=======
-
-    public static void main(String[] args) {
-      
-        IProponente proponente = new IProponente();
-        System.out.println("Hello World");
-        
-    }
->>>>>>> Stashed changes
 }
