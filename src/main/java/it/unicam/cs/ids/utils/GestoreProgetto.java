ec3f288fd05bc89dfb85384ba993ed820ca4bc35
danielepupilli <daniele01.pupilli@studenti.unicam.it>
2020-12-10 11:34:40 +0100

    fix rever error

--- src/main/java/it/unicam/cs/ids/utils/GestoreProgetto.java
+++ src/main/java/it/unicam/cs/ids/utils/GestoreProgetto.java
@@ -1,5 +1,6 @@
 package it.unicam.cs.ids.utils;
-
+import it.unicam.cs.ids.progetto.Candidatura;
+import it.unicam.cs.ids.progetto.StatoCandidatura;
 import it.unicam.cs.ids.progetto.Specializzazione;
 import it.unicam.cs.ids.progetto.Progetto;
 import it.unicam.cs.ids.progetto.StatoProgetto;
@@ -9,6 +10,7 @@ import java.util.*;
 
 public class GestoreProgetto {
     private Set<Progetto> progetti;
+    private Set<Candidatura> candidature;
     public GestoreProgetto(){
         this.progetti = new HashSet<>();
     }
@@ -47,20 +49,49 @@ public class GestoreProgetto {
     public void getListaProgetti(String idProponente, StatoProgetto stato){
         //TODO getListaProgetti
     }
-
-    public void selezionaCandidatura(String idProgetto, StatoProgetto stato){
+    public Set<Candidatura> selezionaCandidatura(String idProgetto, StatoCandidatura stato)
+    {
         //TODO selezionaCandidatura
+        Set<Candidatura>  candidatureCercate = new HashSet<Candidatura>();
+        for(Candidatura candidatura : this.candidature)
+        {
+            if(candidatura.getIdProgetto().equals(idProgetto) && candidatura.getStatoCandidatura().equals(stato))
+            {
+                candidatureCercate.add(candidatura);
+            }
+        }
+        return candidatureCercate;
     }
 
-    public void modificaStatoCandidatura(StatoProgetto stato){
-        //TODO modificaStatoCandidatura
+    public void modificaStatoCandidatura(StatoCandidatura stato)
+    {
+        for (Candidatura candidatura : candidature) 
+        {
+            candidatura.setStatoCandidatura(stato);
+        } 
     }
 
-    public void getInfoProgettisti(List<Progettista> progettisti){
-        //TODO getInfoProgettisti
+
+
+    public void getInfoProgettisti(Set<Progettista> progettisti)
+    {
+        for(Progettista progettista : progettisti)
+        {
+            System.out.println(progettista.getInfo());
+        }
+
     }
 
-    public void notificaEsito(String idProgettista){
-        //TODO notificaEsito
+    public void notificaEsito(String idProgettista)
+    {
+        
+        for(Candidatura candidatura : candidature)
+        {
+            if(candidatura.getIdProgettista().equals(idProgettista))
+            {
+                System.out.println("La tua candidatura è stata:"+ candidatura.getStatoCandidatura());
+            }
+        }
     }
+    
 }
