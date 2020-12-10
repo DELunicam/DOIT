0cd5dfa3e18e6adbdefe6544af81ca08dcdc952e
danielepupilli <daniele01.pupilli@studenti.unicam.it>
2020-12-10 11:09:43 +0100

    inserimento metodi per selezione progettisti

--- src/main/java/it/unicam/cs/ids/DOIT.java
+++ src/main/java/it/unicam/cs/ids/DOIT.java
@@ -1,4 +1,14 @@
 package it.unicam.cs.ids;
 
 public class DOIT {
+<<<<<<< Updated upstream
+=======
+
+    public static void main(String[] args) {
+      
+        IProponente proponente = new IProponente();
+        System.out.println("Hello World");
+        
+    }
+>>>>>>> Stashed changes
 }
