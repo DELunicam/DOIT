add49aeaab053b40ad0bc0f321ea8098a0472ee4
enzodn <enzodn@protonmail.com>
2021-02-22 23:25:29 +0100

    Check inputs viste IUtente IEsperto

--- src/main/java/it/unicam/cs/ids/doit/valutazione/GestoreValutazione.java
+++ src/main/java/it/unicam/cs/ids/doit/valutazione/GestoreValutazione.java
@@ -1,8 +1,11 @@
 package it.unicam.cs.ids.doit.valutazione;
 
+import it.unicam.cs.ids.doit.gestori_utenti.EspertoRepository;
+import it.unicam.cs.ids.doit.progetto.ProgettoRepository;
 import it.unicam.cs.ids.doit.progetto.Specializzazione;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
 
 @Service
@@ -11,6 +14,10 @@ public class GestoreValutazione {
 
     @Autowired
     ValutazioneRepository repository;
+    @Autowired
+    ProgettoRepository progettoRepository;
+    @Autowired
+    EspertoRepository espertoRepository;
 
     public GestoreValutazione() {
 
@@ -23,19 +30,25 @@ public class GestoreValutazione {
         }
         return instance;
     }
-    
+
     // crea valutazione positiva
     public Valutazione createValutazione(Long idProgetto, Long idEsperto, Map<Specializzazione, Integer> infoLavoratori) {
-        Valutazione nuovaValutazione = new Valutazione(idProgetto, idEsperto, infoLavoratori);
-        repository.save(nuovaValutazione);
-        return nuovaValutazione;
+        if (espertoRepository.existsById(idEsperto) && progettoRepository.existsById(idProgetto)) {
+            Valutazione nuovaValutazione = new Valutazione(idProgetto, idEsperto, infoLavoratori);
+            repository.save(nuovaValutazione);
+            return nuovaValutazione;
+        }
+        return null;
     }
 
     // crea valutazione negativa
     public Valutazione createValutazione(Long idProgetto, Long idEsperto) {
-        Valutazione nuovaValutazione = new Valutazione(idProgetto, idEsperto);
-        repository.save(nuovaValutazione);
-        return nuovaValutazione;
+        if (espertoRepository.existsById(idEsperto) && progettoRepository.existsById(idProgetto)) {
+            Valutazione nuovaValutazione = new Valutazione(idProgetto, idEsperto);
+            repository.save(nuovaValutazione);
+            return nuovaValutazione;
+        }
+        return null;
     }
-    
+
 }
